9ca336cfea95ff2b84f971a09cf1194544e8444c
JuanNorena <133806446+JuanNorena@users.noreply.github.com>
2024-02-29 18:33:33 -0500

    Se crearon los metodos de Cliente
    Se agregaron los metodos para crear, eliminar y actualizar cliente

--- src/main/java/co/edu/uniquindio/tienda/controllerModel/ClienteController.java
+++ src/main/java/co/edu/uniquindio/tienda/controllerModel/ClienteController.java
@@ -1,8 +1,24 @@
 package co.edu.uniquindio.tienda.controllerModel;
 
 import co.edu.uniquindio.tienda.controllerModel.service.IClienteControllerService;
+import co.edu.uniquindio.tienda.model.Cliente;
 
 public class ClienteController implements IClienteControllerService {
     ModelFactoryController modelFactoryController;
     public ClienteController(){modelFactoryController = ModelFactoryController.getInstance();}
+
+    @Override
+    public Boolean crearCliente(String numeroIdentificacion, String nombre, String direccion) {
+        return null;
+    }
+
+    @Override
+    public Boolean editarCliente(String numeroIdentificacion, String nombre, String direccion) {
+        return null;
+    }
+
+    @Override
+    public Boolean eliminarCliente(Cliente cliente) {
+        return null;
+    }
 }
--- src/main/java/co/edu/uniquindio/tienda/controllerModel/service/IClienteControllerService.java
+++ src/main/java/co/edu/uniquindio/tienda/controllerModel/service/IClienteControllerService.java
@@ -1,4 +1,9 @@
 package co.edu.uniquindio.tienda.controllerModel.service;
 
+import co.edu.uniquindio.tienda.model.Cliente;
+
 public interface IClienteControllerService {
+    Boolean crearCliente(String numeroIdentificacion, String nombre, String direccion);
+    Boolean editarCliente(String numeroIdentificacion, String nombre, String direccion);
+    Boolean eliminarCliente(Cliente cliente);
 }
